632070ed3f7925e9f7a7a140c800b1fe77746dfb
Mit2001 <mitpshah81@gmail.com>
2022-11-25 00:22:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Details from './Details';
+import Summary from './Summary';
+
+function App() {
+  return (
+    <div className="App">
+      <Details/>
+    </div>
+  );
+}
+
+export default App;
